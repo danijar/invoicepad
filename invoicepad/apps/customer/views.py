fad9c06a989f94d475624351b8adcbf550be24a8
Danijar Hafner <mail@danijar.org>
2014-07-28 15:37:35 +0200

    moved view logic into a function

--- invoicepad/apps/customer/views.py
+++ invoicepad/apps/customer/views.py
@@ -13,7 +13,72 @@ from apps.customer.models import Customer
 from django.db.models.fields.files import ImageFieldFile
 
 
-class MyJSONEncoder(json.JSONEncoder):
+# Respond with serialized entity
+def show(entity, encoder=None):
+    values = model_to_dict(entity)
+    string = json.dumps(values, cls=encoder) if encoder else json.dumps(values)
+    return HttpResponse(string, content_type='application/json')
+
+
+# Update entity from user input
+def update(entity, allowed, values):
+    # Filter allowed values
+    validated = {}
+    for i in allowed:
+        if i in values:
+            validated[i] = values[i]
+    # Update model
+    for (key, value) in validated.items():
+        setattr(entity, key, value)
+    entity.save()
+
+
+def provide(model, request, id, allowed, summary=None, encoder=None):
+    # Require login
+    if request.user.is_anonymous():
+        return HttpResponse('Unauthorized', status=401)
+
+    # Use all for summary if not provided
+    if not summary:
+        summary = allowed
+        summary.append('id')
+
+    # Ensure integer type
+    id = int(id) if id else None
+
+    if request.method == 'GET' and id:
+        # Get single model
+        entity = get_object_or_404(model, id=id, user=request.user)
+        return show(entity, encoder)
+    elif request.method == 'GET':
+        # Get list of all models
+        models = model.objects.filter(user=request.user)
+        values = models.values(*summary)
+        string = json.dumps(list(values))
+        return HttpResponse(string, content_type='application/json')
+    elif request.method == 'POST':
+        # Create new model
+        values = json.loads(request.body.decode('utf-8'))
+        entity = model.objects.create(name='', user=request.user)
+        update(entity, allowed, values)
+        return show(entity, encoder)
+    elif request.method == 'PUT' and id:
+        # Update existing models
+        entity = get_object_or_404(model, id=id, user=request.user)
+        values = json.loads(request.body.decode('utf-8'))
+        update(entity, allowed, values)
+        return show(entity, encoder)
+    elif request.method == 'DELETE' and id:
+        # Delete model
+        entity = get_object_or_404(model, id=id, user=request.user)
+        entity.delete()
+        return HttpResponse()
+    else:
+        # No method available
+        return HttpResponseBadRequest()
+
+
+class Encoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, ImageFieldFile):
             return obj.url if obj else ''
@@ -22,59 +87,6 @@ class MyJSONEncoder(json.JSONEncoder):
 
 @csrf_exempt
 def customer(request, id):
-    if request.user.is_anonymous():
-        return HttpResponse('Unauthorized', status=401)
-
-    # Ensure integer type
-    id = int(id) if id else None
-
-    if request.method == 'GET' and id:
-        # Get single model
-        model = get_object_or_404(Customer, id=id, user=request.user)
-        return show(model)
-    elif request.method == 'GET':
-        # Get list of all models
-        models = Customer.objects.filter(user=request.user)
-        values = models.values('id', 'name', 'website', 'mail', 'logo')
-        string = json.dumps(list(values))
-        return HttpResponse(string, content_type='application/json')
-    elif request.method == 'POST':
-        # Create new model
-        values = json.loads(request.body.decode('utf-8'))
-        model = Customer.objects.create(name='', user=request.user)
-        update(model, values)
-        return show(model)
-    elif request.method == 'PUT' and id:
-        # Update existing models
-        model = get_object_or_404(Customer, id=id, user=request.user)
-        values = json.loads(request.body.decode('utf-8'))
-        update(model, values)
-        return show(model)
-    elif request.method == 'DELETE' and id:
-        # Delete model
-        model = get_object_or_404(Customer, id=id, user=request.user)
-        model.delete()
-        return HttpResponse()
-    else:
-        # No method available
-        return HttpResponseBadRequest()
-
-
-def show(customer):
-    # Respond with serialized model
-    values = model_to_dict(customer)
-    string = json.dumps(values, cls=MyJSONEncoder)
-    return HttpResponse(string, content_type='application/json')
-
-
-def update(customer, values):
-    # Filter allowed values
-    validated = {}
-    for i in ['name', 'fullname', 'address1', 'address2', 'address3', 'mail', 'website', 'notes', 'ustid', 'logo']:
-        if i in values:
-            validated[i] = values[i]
-
-    # Update model
-    for (key, value) in validated.items():
-        setattr(customer, key, value)
-    customer.save()
+    allowed = ['name', 'fullname', 'address1', 'address2', 'address3', 'mail', 'website', 'notes', 'ustid', 'logo']
+    summary = ['id', 'name', 'website', 'mail', 'logo']
+    return provide(Customer, request, id, allowed, summary, encoder=Encoder)
